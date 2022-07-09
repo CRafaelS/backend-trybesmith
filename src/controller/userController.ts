f316192982967523fe94ba57e806b8a9ab7f6847
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2022-07-08 21:25:40 -0300

    feat: validações do user. requisito 3 ok

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,10 +1,15 @@
 import { Request, Response, Router } from 'express';
 import userServices from '../services/userServices';
+import validateUser from '../middleware/validateUser';
 
 const userController = Router();
 
 userController.post(
   '/',
+  validateUser.isValidUsername,
+  validateUser.isValidClasse,
+  validateUser.isValidLevel,
+  validateUser.isValidPassword,
   async (req:Request, res: Response) => {
     const token = await userServices.newUser(req.body);
     return res.status(201).json({ token });
---
+++ src/middleware/validateUser.ts
@@ -0,0 +1,63 @@
+import { NextFunction, Request, Response } from 'express';
+
+const isValidUsername = (req: Request, res: Response, next: NextFunction) => {
+  const { username } = req.body;
+  if (!username || username === undefined) {
+    return res.status(400).json({ message: '"username" is required' });
+  } 
+  if (typeof username !== 'string') {
+    return res.status(422).json({ message: '"username" must be a string' });
+  }
+  if (username.length < 3) {
+    return res.status(422).json({ 
+      message: '"username" length must be at least 3 characters long',
+    });
+  }
+  next();
+};
+
+const isValidClasse = (req: Request, res: Response, next: NextFunction) => {
+  const { classe } = req.body;
+  if (!classe || classe === undefined) {
+    return res.status(400).json({ message: '"classe" is required' });
+  } 
+  if (typeof classe !== 'string') {
+    return res.status(422).json({ message: '"classe" must be a string' });
+  }
+  if (classe.length < 3) {
+    return res.status(422).json({ message: '"classe" length must be at least 3 characters long' });
+  }
+  next();
+};
+
+const isValidLevel = (req: Request, res: Response, next: NextFunction) => {
+  const { level } = req.body;
+  if (level === undefined) {
+    return res.status(400).json({ message: '"level" is required' });
+  } 
+  if (typeof level !== 'number') {
+    return res.status(422).json({ message: '"level" must be a number' });
+  }
+  if (level < 1) {
+    return res.status(422).json({ message: '"level" must be greater than or equal to 1' });
+  }
+  next();
+};
+
+const isValidPassword = (req: Request, res: Response, next: NextFunction) => {
+  const { password } = req.body;
+  if (!password || password === undefined) {
+    return res.status(400).json({ message: '"password" is required' });
+  } 
+  if (typeof password !== 'string') {
+    return res.status(422).json({ message: '"password" must be a string' });
+  }
+  if (password.length < 8) {
+    return res.status(422).json({
+      message: '"password" length must be at least 8 characters long',
+    });
+  }
+  next();
+};
+
+export default { isValidUsername, isValidClasse, isValidLevel, isValidPassword };
